303989a7e411798b54aa0dacf4992962f28d3ec7
adithyanmanayil <115716906+adithyanmanayil@users.noreply.github.com>
2025-01-05 01:43:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,5 +63,5 @@ def colorize():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 4000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host = "0.0.0.0", port=port)
